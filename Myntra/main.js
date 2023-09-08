566588d9ccb0161d46db0146c14b51cd27329027
ZACK0P <119431103+ZACK0P@users.noreply.github.com>
2023-09-08 14:36:26 +0530

    Add files via upload

---
+++ Myntra/main.js
@@ -0,0 +1,112 @@
+/* show menu  */
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// remove menu mobile  
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Scroll sections active link 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.ofsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll',scrollActive)
+
+// Change bg header 
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// show scroll top 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+// mixitup filter portfolio
+const mixer = mixitup('.portfolio__container',{
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+
+// link active portfolio 
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(L => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+
+linkPortfolio.forEach(L => l.addEventListener('click', activePortfolio))
+
+// swiper carousel
+const mySwiper = new Swiper('.testimonial-container', {
+    
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+        pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerview: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+  
+    
+  })
+
+//   GSAP Animation 
+gsap.from('.home__img',{opacity: 0, duration: 2, delay: .5, x:60})
+gsap.from('.home__data',{opacity: 0, duration: 2, delay: .8, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button',{opacity: 0, duration: 2, delay: 1, y:25, ease:'expo.out, stagger: .2'})
+
+gsap.from('.nav__logo, .nav__toggle',{opacity: 0, duration: 2, delay: 1.5, y:25, ease:'expo.out, stagger: .2'})
+gsap.from('.nav__item',{opacity: 0, duration: 2, delay: 1.8, y:25, ease:'expo.out, stagger: .2'})
+gsap.from('.home__social-icon',{opacity: 0, duration: 2, delay: 2.3, y:25, ease:'expo.out, stagger: .2'})
